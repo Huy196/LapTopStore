c3b2dadbec8f33d4c0aa277c464562967f302943
doquochuy <doquochuyy66@gmail.com>
2024-10-15 10:15:02 +0700

    ConnectionDatabse

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConnectionDatabase connectionDatabase = new ConnectionDatabase();
+        connectionDatabase.connection();
     }
 }
